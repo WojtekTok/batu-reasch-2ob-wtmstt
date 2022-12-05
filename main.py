9f5605b2470196af3ef64ec0c070ee4c0819d1fd
Wojciech <wtokarz@student.agh.edu.pl>
2022-12-05 19:26:34 +0100

    generowanie bazowego rozwiązania, trzebe dodać ograniczenia i sensownie spiąć te dwie klasy bo Factory ma przechowywać najlepsze rozwiązanie

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import random
 
 
 class Factory:
-    default_hps = np.array([0, 15, 25, 0],
+    default_hps = np.array([[0, 15, 25, 0],
                            [12, 6, 0, 14],
-                           [10, 8, 0, 0])
+                           [10, 8, 0, 0]])
 
     default_profit = (600, 700, 500, 300)
 
@@ -29,7 +29,7 @@ class Factory:
         self.machines_per_stage = machines_per_stage
         self.limits_per_machine = limits_per_machine
         self.checking_time = checking_time
-        self.production = self.random_solution()  # tu będzie wywoływana funckja do wyliczenia pierwszego rozwiązania - lista z intami
+        # self.best_solution = Solution()
 
     def funkcja_celu(self):
         """
@@ -57,15 +57,26 @@ class Solution(Factory):
                  machines_per_stage=Factory.default_mps, limits_per_machine=Factory.default_lpm, checking_time=10)
         self.workers_hours_left = self.workers_hours
         self.hours_per_machine_left = list(self.limits_per_machine)
+        self.production = np.zeros(len(self.profit))
 
     def random_solution(self):
-        while self.workers_hours > 0: # TODO:dodać że jeśli nie znajdzie nic więcej niż ileśtam razy to też przerywa petle
+        while self.workers_hours_left > 0: # TODO:dodać że jeśli nie znajdzie nic więcej niż ileśtam razy to też przerywa petle
             self.random_part()
 
     def random_part(self):
-        part_number = random.randint(0, self.hours_per_stage.shape[1])
-        for i in range(self.hours_per_stage[0]):
-            if self.hours_per_stage[i][part_number] > self.hours_per_machine_left[i] or self.hours_per_stage[i][part_number] > self.workers_hours_left:
-                self.hours_per_machine_left[i] -= self.hours_per_stage[i][part_number]
-                self.workers_hours_left -= self.hours_per_stage[i][part_number]
+        part_number = random.randint(0, self.hours_per_stage.shape[1]-1)
+        if np.sum(self.hours_per_stage[:, part_number]) + self.checking_time > self.workers_hours_left:
+            return
+        for i in range(self.hours_per_stage.shape[0]):
+            if self.hours_per_stage[i, part_number] > self.hours_per_machine_left[i]:
+                return
+        for i in range(self.hours_per_stage.shape[0]):
+            self.hours_per_machine_left[i] -= self.hours_per_stage[i, part_number]
+            self.workers_hours_left -= self.hours_per_stage[i, part_number]
+        self.workers_hours_left -= self.checking_time
         self.production[part_number] += 1  # dodaje przedmiot do wektora rozwiązań
+
+
+sol = Solution()
+sol.random_solution()
+print()
