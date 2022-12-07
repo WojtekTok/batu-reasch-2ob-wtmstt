c3849615d14e3fe10e7c9bb3c411635bf17c9fd1
michalsciubisz <michalsci@student.agh.edu.pl>
2022-12-07 15:14:49 +0100

    update ograniczen + zmienione klasy
    dodałem poprawione ograniczenia, plus zmodyfikowałem te dwie klasy jak to widziałem, ale są problemy żeby to zaimplementowac więc odkładam to na razie, bo wszystkie się sypię w tym Solution

--- main.py
+++ main.py
@@ -2,59 +2,60 @@ import numpy as np
 import random
 
 class Product:
-    def __init__(self, product_id, profit, time_1, time_2, time_3) -> None:
+    def __init__(self, profit, amount_of_mt):
         """
         Uwzględniamy stałą liczbę maszyn, jednak zmienną ilość produktów. 
-        :param product_id: pozwala na identyfikację produktu na podstawie ID
         :param profit: pozwala na zdefiniowanie przychodu za daną część
-        :param time_1: czas wymagany na pierwszym rodzaju maszyny
-        :param time_2: czas wymagany na drugim rodzaju maszyny
-        :param time_3: czas wymagany na trzecim rodzaju maszyny
+        :param amount_of_mt: ilość rodzajów maszyn, bezpośrednio związana z klasą Machines
         """
-        self.product_id = product_id
         self.profit = profit
-        self.time_1 = time_1
-        self.time_2 = time_2
-        self.time_3 = time_3
+        self.amount_of_mt = amount_of_mt
 
-    def hps(self):
+    def hps(self, *args):
         """
         funkcja zwracająca listę godzin potrzebnych na poszczególne maszyny
         :return: zwracana jest lista, która zawiera czas potrzebny do produkcji produktu na poszczególnych etapie
         """
-        return [self.time_1, self.time_2, self.time_3]
+        time_vector = list(args)
+        if len(time_vector) == self.amount_of_mt:
+            return np.transpose(time_vector)
+        else:
+            return 0
+
+    def hps_matrix(self, *args):
+        """
+        funkcja łączy podane w parametrach wektory w macierz, w tym przypadku łączymy wektory transponowane z funkcji hps
+        :return: zwraca macierz wektorów, które określają czas potrzebny na poszczególny etap
+        """
+        return np.column_stack(args)
+
+    def profit_all_products(self, *args):
+        """
+        funkcja zwraca krotkę, zawierającą przychód osiągalny z poszczególnych części
+        :return: zwraca krotkę, zawierającą przychód z poszczególnych części
+        """
+        return args
+
 
 class Machines:
-    def __init__(self, amount_1, amount_2, amount_3, max_time_1, max_time_2, max_time_3) -> None:
+    def __init__(self, types):
         """
-        Zakładamy, że istnieją maksymalnie 3 rodzaje maszyn.
-        :param amount_1: ilość maszyn 1-go rodzaju
-        :param amount_2: ilość maszyn 2-go rodzaju
-        :param amount_3: ilość maszyn 3-go rodzaju
-        :param max_time_1: maksymalny czas pracy maszyn 1-go rodzaju
-        :param max_time_2: maksymalny czas pracy maszyn 1-go rodzaju
-        :param max_time_3: maksymalny czas pracy maszyn 1-go rodzaju
+        Zakładamy, że istnieją zmienna ilość rodzajów maszyn
+        :param types: mówi nam ile rodzajów maszyn uwzględniamy
         """
-        self.amount_1 = amount_1
-        self.amount_2 = amount_2
-        self.amount_3 = amount_3
-        self.max_time_1 = max_time_1
-        self.max_time_2 = max_time_2
-        self.max_time_3 = max_time_3
+        self.types = types
 
-    def mps(self):
+    def amount_of_specific_type(self, *args):
         """
-        funkcja zwracająca ilość maszyn na danym etapie
-        :return: krotka zwracająca ilość maszyn na poszczególnym etapie
+        funkcja pozwalająca na wprowadzenie ilości urządzeń danego rodzaju dostępnych w fabrycę
+        :return: zwraca krotkę, liczb maszyn dostępnych na podanych kolejno etapach, zwraca 0 w przypadku gdy nie podano, wartości któregoś z parametrów lub podano ich za dużo
         """
-        return (self.amount_1, self.amount_2, self.amount_3)
+        amount_tuple = args
+        if len(amount_tuple) == self.types:
+            return amount_tuple
+        else:
+            return 0
 
-    def lpm(self):
-        """
-        funkcja zwracająca maksymalny czas pracy na poszczególnych etapach
-        :return: krotka zawierająca maksymalny czas pracy na poszczególnych etapach
-        """
-        return (self.max_time_1, self.max_time_2, self.max_time_3)
 
 class Factory():
     default_hps = np.array([[0, 15, 25, 0],
@@ -98,22 +99,20 @@ class Factory():
     def ograniczenia(self):
         """
         funkcja sprawdzająca czy zachowane są odgórnie narzucone ograniczenia
-        :return: zwraca błąd gdy niezachowane jest dane ograniczenie
+        :return: zwraca błąd gdy niezachowane jest dane ograniczenie, 1 oznacza rozwiązanie niedopuszczalne, 0 rozwiązanie dopuszczonalne
         """
-        x, y = self.hours_per_stage.shape
         requirements = np.zeros(len(self.profit))
         req_worker = 0
 
-        for i in range(x):
-            for j in range(y):
+        for i in range(self.hours_per_stage.shape[0]):
+            for j in range(self.hours_per_stage.shape[1]):
                 requirements[i] += self.hours_per_stage[i][j] * self.production[j]
-                if requirements[i] > self.limits_per_machine:
-                    raise SystemError('Maszyna będzie pracować zbyt długo!') # TODO: zamienić SystemError na jakiś inny error, który łatwo "przechwycić
-        
-        for i in range(x):
-            req_worker += self.checking_time * self.production[i]
+                if requirements[i] > 5 * 8 * self.machines_per_stage[i]: # 5 (dni tygodnia) * 8 (roboczogodzin dziennie) * ilość maszyn na danym etapie
+                    return 1
+            req_worker += self.checking_time * self.production[i] + requirements[i]
             if req_worker > self.workers_hours:
-                raise SystemError('Nie ma tyle dostępnych godzin')
+                return 1
+        return 0
 
 
 
@@ -156,10 +155,27 @@ class Solution(Factory):
         self.workers_hours_left -= self.checking_time
         self.production[part_number] += 1  # dodaje przedmiot do wektora rozwiązań
 
-    
+#Zdefiniowanie ilości maszyn
+mach = Machines(3)
+machines_per_stage = mach.amount_of_specific_type(3, 3, 2)
+
+#Zdefiniowanie produktów
+prod1 = Product(600, mach.types)
+vec1 = prod1.hps(0, 12, 10)
+prod2 = Product(700, mach.types)
+vec2 = prod2.hps(15, 6, 8)
+prod3 = Product(500, mach.types)
+vec3 = prod3.hps(25, 0, 0)
+prod4 = Product(300, mach.types)
+vec4 = prod4.hps(0, 14, 0)
+
+#Przygotowanie potrzebnych macierzy i wektorów
+hps_matrix = prod1.hps_matrix(vec1, vec2, vec3, vec4)
+profits = prod1.profit_all_products(prod1.profit, prod2.profit, prod3.profit, prod4.profit)
 
 sol = Solution()
 sol.random_solution()
 print(sol.funkcja_celu())
+print(sol.ograniczenia())
 print(sol.production)
 print(sol.best_production)
